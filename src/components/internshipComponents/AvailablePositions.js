8a2c0a0b009c012e1d4ba9a3c090b58729571a82
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-10-16 12:59:16 -0400

    Created function to dynamically create positions

--- src/components/internshipComponents/AvailablePositions.js
+++ src/components/internshipComponents/AvailablePositions.js
@@ -1,53 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button} from 'react-bootstrap';
-import websiteDesigner from "../../images/Internships/WebsiteDesigner.jpg";
-import grantWriter from "../../images/Internships/GrantWriter.jpg";
-import digitalMarketingOfficer from "../../images/Internships/DigitalMarketingOfficer.jpg";
-import financeDirector from "../../images/Internships/FinanceDirector.jpg";
-import outreachCoordinator from "../../images/Internships/OutreachCoordinator.jpg";
-import marketingSpecialist from "../../images/Internships/MarketingSpecialist.jpg";
-import photographer from "../../images/Internships/Photographer.jpg";
-import legalAffairs from "../../images/Internships/LegalAffairs.jpg";
-import vicePresidentExternal from "../../images/Internships/VicePresidentExternal.jpg";
-import academicAffairs from "../../images/Internships/AcademicAffairs.jpg";
-import publicRelations from "../../images/Internships/PublicRelations.jpg";
-import humanResources from "../../images/Internships/HumanResources.jpg";
-import careerAndProfessionalDevelopment from "../../images/Internships/CareerAndProfessionalDevelopment.jpg";
-import sportsCordinator from "../../images/Internships/SportsCordinator.jpg";
-import communicationDirector from "../../images/Internships/CommunicationDirector.jpg";
-import vicePresidentInternal from "../../images/Internships/VicePresidentInternal.jpg";
-import videographer from "../../images/Internships/Videographer.jpg";
-import videoEditor from "../../images/Internships/VideoEditor.jpg";
-import {BrowserRouter as Router} from 'react-router-dom'
-import { NavLink, Link } from 'react-router-dom';
-import Route from 'react-router-dom'
-import { browserHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "../CSS/AvailablePosition.css";
 
 export default function AvailablePositions() {
 /**
  * @var {openPositions} String - Position names that are currently open
- * @function filterPosition - filter positions and add hidden property to the ones not active/open
- * Pending: function works, but page is not re-rendering 
+ * .map checks every item/position in the array above and create a component for it.
  */
 
-    const [position, setPosition] = useState([]
-        )
-
-        useEffect(()=>{
-            filterPosition()
-        }, [])
-    function filterPosition(){
-    let cards = document.querySelectorAll('.card.position > p');
-    let openPositions = ["Website Designer"];
-    cards.forEach(item =>{
-        if(openPositions.includes(item.textContent)) {
-            console.log('exists')
-        } else {
-            item.parentElement.classList.add('d.none')
-        }
-    })
-}
+let openPositions = ["Website Designer", "Grant Writer", "Finance Director"];
 
         return(
             <div style={{ textAlign: "center", margin:"30px"}}>
@@ -56,140 +16,22 @@ export default function AvailablePositions() {
                     </p>
                     <div style={{ border: "0.1px solid #ccc", width: "50px", margin: "auto" }}></div>
                     <div id="AvailablePositions">
-                        <div className='card position'>
-                            <p>Website Designer</p>
-                            <img src={websiteDesigner} alt='web designer'/>
-                            <div className="content">
-                            <Link to="/websiteDesigner">
-                            <button>FIND  OUT  MORE</button>
-                            </Link>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Grant Writer</p>
-                            <img src={grantWriter} alt='writer'/>
-                            <div className="content">
-                            <Link to="/GrantWriter">
-                            <button>FIND  OUT  MORE</button>
-                            </Link>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Digital Marketing Officer</p>
-                            <img src={digitalMarketingOfficer} alt='digital marketing officer'/>
-                            <div className="content">
-                            <Link to="/DigitalMarketing">
-                            <button>FIND  OUT  MORE</button>
-                            </Link>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Finance Director</p>
-                            <img src={financeDirector} alt='finance director'/>
-                            <div className="content">
-                            <Link to="/FinanceDirector">
-                            <button>FIND  OUT  MORE</button>
-                            </Link>   
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Outreach Coordinator</p>
-                            <img src={outreachCoordinator} alt='outreach coordinator'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Marketing Specialist</p>
-                            <img src={marketingSpecialist} alt='marketing specialist'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Photographer</p>
-                            <img src={photographer} alt='photographer'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Legal Affairs</p>
-                            <img src={legalAffairs} alt='legal affairs'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Vice-President External</p>
-                            <img src={vicePresidentExternal} alt='VP external'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Academic Affairs</p>
-                            <img src={academicAffairs} alt='academic affairs'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Public Relations</p>
-                            <img src={publicRelations} alt='Public Relations'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Human Resources</p>
-                            <img src={humanResources} alt='Human resources'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Professional Development Officer</p>
-                            <img src={careerAndProfessionalDevelopment} alt='Professional Development Officer'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Sports Coordinator</p>
-                            <img src={sportsCordinator} alt='Sports Coordinator'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Communication Director</p>
-                            <img src={communicationDirector} alt='Communication Director'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Vice-President Internal</p>
-                            <img src={vicePresidentInternal} alt='VP Internal'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Videographer</p>
-                            <img src={videographer} alt='videographer'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
-                        <div className="card position">
-                            <p>Video Editor</p>
-                            <img src={videoEditor} alt='video editor'/>
-                            <div className="content">
-                            <button>FIND  OUT  MORE</button>
-                            </div>
-                        </div>
+                    {
+                        openPositions.map((item, index) => {
+                            let str = item.replace( /\s/g, '');
+                            return(
+                                <div className="card" key={index}>
+                                <p className='position'>{item}</p>
+                                <img src={require(`../../images/Internships/${str}.jpg`).default} alt={item}/>
+                                <div className="content">
+                                <Link to={"/"+ str}>
+                                <button>FIND  OUT  MORE</button>
+                                </Link>
+                                </div>
+                            </div>)
+                        }
+
+                        )}
                     </div>
             </div>        
         )
