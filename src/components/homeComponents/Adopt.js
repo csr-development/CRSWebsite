191e14afc21dba7095abb98785fdf9a37989f8fe
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-10-16 10:30:01 -0400

    Fixed Adopt font/overlay/contrast

--- src/components/homeComponents/Adopt.js
+++ src/components/homeComponents/Adopt.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import hand from '../../images/helping-hand.jpg';
 import { Button } from 'react-bootstrap';
+import './adopt.css';
 
 export default function Adopt() {
     return (
-        <div style={{
-            background: `url(${hand}) center no-repeat`,
-            backgroundSize: "cover",
-            color: "black",
-        }}>
-            <h1 style={{ textAlign: "center", fontSize: "45px", paddingTop: "75px" }}>ADOPT A STUDENT CAMPAIGN</h1>
+        <div className='adopt'>
+            <div className='adopt-text'>
+            <h1 id='adopt-text' style={{ textAlign: "center", fontSize: "45px", paddingTop: "75px" }}>ADOPT A STUDENT CAMPAIGN</h1>
             <div style={{ textAlign: "center", paddingBottom: "50px" }}><Button variant="danger">LEARN MORE</Button></div>
+            </div>
         </div>
     )
 }
--- src/components/homeComponents/Together.js
+++ src/components/homeComponents/Together.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import people from '../../images/people.jpg';
 import { Button } from 'react-bootstrap';
+import "./together.css";
+
 
 export default function Together() {
     return (
-        <div style={{
-            background: `url(${people}) center no-repeat fixed`,
-            backgroundSize: "cover",
-            width: '100vw',
-        }}>
+        <div className='together'>
+            <div className='together-text'>
             <h1 style={{ textAlign: "center", fontSize: "70px", paddingTop: "150px" }}>Together, we can.</h1>
             <h4 style={{ textAlign: "center" }}>By the students, for the students.</h4>
             <div style={{ textAlign: "center", paddingBottom: "100px" }}><Button variant="danger">JOIN US</Button></div>
+            </div>
         </div >
     )
 }
